4ff9ce61605632b4e411a8197e2523ed6befc08e
miguel-mpm <60815420+miguel-mpm@users.noreply.github.com>
2025-05-17 13:21:58 +0200

    Update nochat4u.rb

--- Casks/nochat4u.rb
+++ Casks/nochat4u.rb
@@ -2,7 +2,7 @@ cask "nochat4u" do
   desc "NoChat4U Application"
   homepage "https://github.com/miguel-mpm/NoChat4U"
   url "https://github.com/miguel-mpm/NoChat4U/releases/download/v1.0.0/NoChat4U-v1.0.0.zip"
-  sha256 "bfcbad4ccb5fb1dd6421cb073429ba3049470cb48e315ae6e965511ec1b48052"
+  sha256 "269267b68658d393bbde97e6b883c41fb6d82d459dad92eadf79d3d8f5e4a1f6"
   version "1.0.0"
 
   app "NoChat4U.app"
